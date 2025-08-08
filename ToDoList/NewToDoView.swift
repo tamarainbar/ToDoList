ca397c713847b9de16a0f6d4f1100a1a3fb77142
Tamara Inbar <tamarainbar@gmail.com>
2025-08-08 13:18:58 -0400

    added adding a task feature

--- ToDoList/NewToDoView.swift
+++ ToDoList/NewToDoView.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 struct NewToDoView: View {
     var body: some View {
-        Text("Task Title:")
-        TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-            .multilineTextAlignment(.center)
+        VStack {
+            Spacer()
+            Text("Task Title:")
+            TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .frame(width: 370.0, height: 30.0)
+                .multilineTextAlignment(.center)
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(10)
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("Is it important")
+            }
+            Button {
+                
+            } label: {
+                Text("Save")
+            }
+        } //VStack
         .padding()
     }
 }
